3f4eaeeb1cfb5f616a6a3102476bd4e95db189c0
Erwan Le Huitouze <erwan.lehuitouze@tactfactory.com>
2015-11-08 19:42:02 +0100

    Apply new package architecture

--- src/main/java/org/rosmultimedia/player/xbmc/internal/XbmcSpeaker.java
+++ src/main/java/org/rosmultimedia/player/xbmc/internal/XbmcSpeaker.java
@@ -28,95 +28,95 @@ import media_msgs.ToggleMuteSpeakerResponse;
  *
  */
 public class XbmcSpeaker implements ISpeaker {
-	/**
-	 * Xbmc node.
-	 */
-	private XbmcNode xbmcNode;
+    /**
+     * Xbmc node.
+     */
+    private XbmcNode xbmcNode;
 
-	/**
-	 * Xbmc json-rpc.
-	 */
-	private XbmcJson xbmcJson;
+    /**
+     * Xbmc json-rpc.
+     */
+    private XbmcJson xbmcJson;
 
-	/**
-	 * XbmcSpeaker constructor.
-	 * @param xbmcJson {@link XbmcJson} xbmc json-rpc
-	 * @param xbmcNode {@link XbmcNode} xbmc node
-	 */
-	public XbmcSpeaker(XbmcJson xbmcjson, XbmcNode xbmcNode) {
-		this.xbmcJson = xbmcjson;
-		this.xbmcNode = xbmcNode;
-	}
+    /**
+     * XbmcSpeaker constructor.
+     * @param xbmcJson {@link XbmcJson} xbmc json-rpc
+     * @param xbmcNode {@link XbmcNode} xbmc node
+     */
+    public XbmcSpeaker(XbmcJson xbmcjson, XbmcNode xbmcNode) {
+        this.xbmcJson = xbmcjson;
+        this.xbmcNode = xbmcNode;
+    }
 
-	@Override
-	public void load(StateData stateData) {
-	    this.load(stateData.getSpeaker());
-	}
+    @Override
+    public void load(StateData stateData) {
+        this.load(stateData.getSpeaker());
+    }
 
-	public void load(SpeakerInfo speakerInfo) {
-		PropertyValue property = this.xbmcJson.getResult(
-				new Application.GetProperties(
-						PropertyValue.MUTED,
-						PropertyValue.NAME,
-						PropertyValue.VERSION,
-						PropertyValue.VOLUME));
+    public void load(SpeakerInfo speakerInfo) {
+        PropertyValue property = this.xbmcJson.getResult(
+                new Application.GetProperties(
+                        PropertyValue.MUTED,
+                        PropertyValue.NAME,
+                        PropertyValue.VERSION,
+                        PropertyValue.VOLUME));
 
-		if (property != null) {
-    		speakerInfo.setMuted(property.muted);
-    		speakerInfo.setLevel(property.volume);
-		}
-	}
+        if (property != null) {
+            speakerInfo.setMuted(property.muted);
+            speakerInfo.setLevel(property.volume);
+        }
+    }
 
-	@Override
-	public void callbackCmdAction(MediaAction message, StateData stateData) {
-		switch (message.getMethod()) {
-			case OP_MUTE:
-			case OP_MUTE_TOGGLE:
-				this.xbmcJson.getResult(new Application.SetMute(
-						new Toggle(!stateData.getSpeaker().getMuted())));
-				break;
-			case OP_VOLUME_DOWN:
-				int level = stateData.getSpeaker().getLevel() - LEVEL_STEP;
+    @Override
+    public void callbackCmdAction(MediaAction message, StateData stateData) {
+        switch (message.getMethod()) {
+            case OP_MUTE:
+            case OP_MUTE_TOGGLE:
+                this.xbmcJson.getResult(new Application.SetMute(
+                        new Toggle(!stateData.getSpeaker().getMuted())));
+                break;
+            case OP_VOLUME_DOWN:
+                int level = stateData.getSpeaker().getLevel() - LEVEL_STEP;
 
-				if (level < LEVEL_MIN) {
-					level = LEVEL_MIN;
-				}
+                if (level < LEVEL_MIN) {
+                    level = LEVEL_MIN;
+                }
 
-				this.xbmcJson.getResult(new Application.SetVolume(level));
-				break;
-			case OP_VOLUME_UP:
-				level = stateData.getSpeaker().getLevel() + LEVEL_STEP;
+                this.xbmcJson.getResult(new Application.SetVolume(level));
+                break;
+            case OP_VOLUME_UP:
+                level = stateData.getSpeaker().getLevel() + LEVEL_STEP;
 
-				if (level > LEVEL_MAX) {
-					level = LEVEL_MAX;
-				}
+                if (level > LEVEL_MAX) {
+                    level = LEVEL_MAX;
+                }
 
-				this.xbmcJson.getResult(new Application.SetVolume(level));
-				break;
-			case OP_VOLUME_TO:
-				level = 50;
+                this.xbmcJson.getResult(new Application.SetVolume(level));
+                break;
+            case OP_VOLUME_TO:
+                level = 50;
 
-				if (level > LEVEL_MIN && level < LEVEL_MAX) {
-					this.xbmcJson.getResult(new Application.SetVolume(level));
-				}
-				break;
-		}
-	}
+                if (level > LEVEL_MIN && level < LEVEL_MAX) {
+                    this.xbmcJson.getResult(new Application.SetVolume(level));
+                }
+                break;
+        }
+    }
 
-	@Override
-	public void handleSpeakerMuteToggle(ToggleMuteSpeakerRequest request,
-			ToggleMuteSpeakerResponse response) {
-		response.setState(!this.xbmcNode.getStateData().getSpeaker().getMuted());
+    @Override
+    public void handleSpeakerMuteToggle(ToggleMuteSpeakerRequest request,
+            ToggleMuteSpeakerResponse response) {
+        response.setState(!this.xbmcNode.getStateData().getSpeaker().getMuted());
 
-		this.xbmcNode.logI(String.format("Service call %s : %s",
-				XbmcNode.SRV_MUTE_SPEAKER_TOGGLE,
-				this.xbmcNode.getStateData().getSpeaker().getMuted()));
+        this.xbmcNode.logI(String.format("Service call %s : %s",
+                XbmcNode.SRV_MUTE_SPEAKER_TOGGLE,
+                this.xbmcNode.getStateData().getSpeaker().getMuted()));
 
-		MediaAction message = this.xbmcNode.getConnectedNode().getTopicMessageFactory()
-				.newFromType(MediaAction._TYPE);
+        MediaAction message = this.xbmcNode.getConnectedNode().getTopicMessageFactory()
+                .newFromType(MediaAction._TYPE);
 
-		message.setMethod(XbmcSpeaker.OP_MUTE_TOGGLE);
+        message.setMethod(XbmcSpeaker.OP_MUTE_TOGGLE);
 
-		this.callbackCmdAction(message, this.xbmcNode.getStateData());
-	}
+        this.callbackCmdAction(message, this.xbmcNode.getStateData());
+    }
 }
